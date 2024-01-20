b8e610ac8c0b423e369779a8236e9dc9267ec680
Mateusz Bartnicki <m.bartnicki38@gmail.com>
2024-01-20 12:31:10 +0100

    Added basic configuration for application.

---
+++ src/main/java/pl/Aevise/SupperSpeed/SupperSpeedApplication.java
@@ -0,0 +1,12 @@
+package pl.Aevise.SupperSpeed;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupperSpeedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SupperSpeedApplication.class, args);
+    }
+}
