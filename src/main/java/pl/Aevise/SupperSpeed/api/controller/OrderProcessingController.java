7abb649ad2d5dc57ad0ad92050e5b2865c503cb3
Mateusz Bartnicki <m.bartnicki38@gmail.com>
2024-04-05 14:22:23 +0200

    changing order creation

---
+++ src/main/java/pl/Aevise/SupperSpeed/api/controller/OrderProcessingController.java
@@ -0,0 +1,11 @@
+package pl.Aevise.SupperSpeed.api.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+public class OrderProcessingController {
+
+    static final String ORDER_PROCESSING = "/orders/submit-order";
+}
