f1633abb08f774825f00f66aabfadcdc72513643
Mateusz Bartnicki <mateusz.bartnicki@pitradwar.com>
2024-04-19 09:43:19 +0200

    dropped menu idea

--- src/main/java/pl/Aevise/SupperSpeed/infrastructure/database/repository/jpa/MenuJpaRepository.java
+++ src/main/java/pl/Aevise/SupperSpeed/infrastructure/database/repository/jpa/MenuJpaRepository.java
@@ -1,7 +1,6 @@
 package pl.Aevise.SupperSpeed.infrastructure.database.repository.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pl.Aevise.SupperSpeed.infrastructure.database.entity.MenuEntity;
 
 public interface MenuJpaRepository extends JpaRepository<MenuEntity, Integer> {
 }
