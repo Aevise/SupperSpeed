3796e1549f44f8ab182fc5a1f18255ebb94c3e08
Mateusz Bartnicki <mateusz.bartnicki@pitradwar.com>
2024-05-14 07:44:54 +0200

    .

--- src/main/java/pl/Aevise/SupperSpeed/business/GeocodingService.java
+++ src/main/java/pl/Aevise/SupperSpeed/business/GeocodingService.java
@@ -9,11 +9,11 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Service
 public class GeocodingService {
 
-    @Value("${google.api.key}")
-    private String apiKey;
-
-    @Value("${google.geocode.url}")
-    private String googleGeocodeUrl;
+//    @Value("${google.api.key}")
+//    private String apiKey;
+//
+//    @Value("${google.geocode.url}")
+//    private String googleGeocodeUrl;
 
     private final RestTemplate restTemplate;
 
@@ -22,11 +22,12 @@ public class GeocodingService {
     }
 
     public String getGeocode(String address) {
-        String urlTemplate = UriComponentsBuilder.fromHttpUrl(googleGeocodeUrl)
-                .queryParam("address", address)
-                .queryParam("key", apiKey)
-                .toUriString();
-
-        return restTemplate.getForObject(urlTemplate, String.class);
+//        String urlTemplate = UriComponentsBuilder.fromHttpUrl(googleGeocodeUrl)
+//                .queryParam("address", address)
+//                .queryParam("key", apiKey)
+//                .toUriString();
+//
+//        return restTemplate.getForObject(urlTemplate, String.class);
+        return "";
     }
 }
